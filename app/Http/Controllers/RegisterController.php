ca2d78c459ae9e873e56542b71f831115ebd9133
Ruslan.Shamsudinov <ruslan.shamsudinov.217@gmail.com>
2024-07-02 22:40:50 +0400

    Was tested

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -14,19 +14,27 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
-        $validated = $request->validate([
-            'name' => ['required', 'string', 'max:50'],
-            'email' => ['required', 'string', 'max:50', 'email', 'unique:users'],
-            'password' => ['required', 'string', 'min:7', 'max:50', 'confirmed'],
-            'agreement' => ['accepted'],
-        ]);
 
-        User::query()->create([
-            'name' => $validated['name'],
-            'email' => $validated['email'],
-            'password' => bcrypt($validated['password']),
-        ]);
+        $request = request()->has('number');
+        dd($request);
 
-        return redirect()->route('user');
+
+
+//        return 'Sign Up Request';
+
+//        $validated = $request->validate([
+//            'name' => ['required', 'string', 'max:50'],
+//            'email' => ['required', 'string', 'max:50', 'email', 'unique:users'],
+//            'password' => ['required', 'string', 'min:7', 'max:50', 'confirmed'],
+//            'agreement' => ['accepted'],
+//        ]);
+//
+//        User::query()->create([
+//            'name' => $validated['name'],
+//            'email' => $validated['email'],
+//            'password' => bcrypt($validated['password']),
+//        ]);
+//
+//        return redirect()->route('user');
     }
 }
